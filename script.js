3760d901aea530fcb8f4057e8fe1864f4e1f3430
gabrielf <gforestiero@alumno.unlam.edu.ar>
2024-07-03 00:13:03 -0300

    graficos

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ burger.addEventListener('click', () => {
     burger.classList.toggle('toggle');
 });
 
-
 function enviarMensaje() {
     const mensajeInput = document.getElementById('mensaje');
     const mensaje = mensajeInput.value;
@@ -21,7 +20,47 @@ function enviarMensaje() {
       mensajeInput.value = '';
       chatWidget.scrollTop = chatWidget.scrollHeight;
     }
-  }
-  
+}
 
-  
+document.addEventListener('DOMContentLoaded', function() {
+
+    const datos = {
+      labels: ['Menores de 18', '18-25', '26-35', '36-45', 'Más de 45'],
+      datasets: [{
+        label: 'Edades',
+        data: [15, 30, 45, 25, 10], 
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)'
+        ],
+        borderWidth: 1
+      }]
+    };
+
+    const config = {
+      type: 'bar',
+      data: datos,
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      }
+    };
+
+    var myChart = new Chart(
+      document.getElementById('ageGenderChart'),
+      config
+    );
+});
